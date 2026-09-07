76e73a20e5d8534e08a3a22d4bf4925f6e901a09
snehasishbeura <snehasishatwork@gmail.com>
2026-09-07 22:21:54 +0530

    feat: update config.py with LOL dataset paths and pipeline parameters

---
+++ config.py
@@ -0,0 +1,6 @@
+DATASET_LOW  = r"C:\Users\notsnehasis\Downloads\LOLdataset\eval15\low"
+DATASET_HIGH = r"C:\Users\notsnehasis\Downloads\LOLdataset\eval15\high"
+
+GAMMA = 1.8
+
+OUTPUT_FOLDER = "output"
